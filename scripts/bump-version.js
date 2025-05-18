747492e2852f5ed309520f3c62894eea4437817f
uxp <uxp@hplogsdon.com>
2025-05-17 23:12:08 -0600

    Add version bumping script

---
+++ scripts/bump-version.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const { writeFileSync, readFileSync, existsSync } = require('fs');
+const {inc} = require("semver");
+
+const {Command} = require('commander');
+const program = new Command()
+
+function fatal(msg) {
+    console.error();
+    console.error('    ' + msg);
+    console.error();
+    process.exit(1);
+}
+
+function bump(root, type) {
+    const packagePath = path.resolve(root, "./package.json");
+    if (!existsSync(packagePath)) {
+        throw new Error("Unable to locate package.json");
+    }
+    const packageJson = JSON.parse(readFileSync(packagePath, 'utf-8'));
+    const version = packageJson.version;
+    if (!version) {
+        throw new Error("No version found in package.json")
+    }
+
+    const newVersion = inc(packageJson.version, type);
+    if (!newVersion) {
+        throw new Error(`Unable to bump version number from ${version} using ${type}`);
+    }
+    packageJson.version = newVersion;
+    writeFileSync(packagePath, JSON.stringify(packageJson, null, 2) + '\n');
+
+    return packageJson.version;
+}
+
+program
+    .usage("[options] <version>")
+    .description("Bumps version number.")
+    .argument("<version>", "semver version or type (major, premajor, minor, preminor, patch, prepatch, prerelease, or release)")
+    .option('-d', '--dir <dir>', 'determine the directory to use as the root')
+    .parse(process.argv);
+
+if (program.args.length !== 1) program.help();
+
+let info = undefined;
+try {
+    const dir = program.dir || path.resolve(__dirname, '../');
+    const version = program.args.shift();
+
+    info = bump(dir, version);
+} catch (e) {
+    fatal(e.message);
+}
+
+console.log('Version bumped to "' + info + '".');
